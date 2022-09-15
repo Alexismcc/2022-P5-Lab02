7d0f2428b16a966793d8307649ced74e5b0a0fb0
Alexis <alexis.mccarthy@vtsd.com>
2022-09-15 12:02:28 -0400

    Second Lab

--- Lab02vst.java
+++ Lab02vst.java
@@ -1,7 +1,24 @@
 public class Lab02vst {
 
-    public static void main(String args[]){
-
+    public static void main(String args[]) {
+        System.out.println("lab 02, 80 point version");
+        System.out.println();
+        System.out.println();
+        System.out.print("Measure today's knowledge ");
+        System.out.println("by yesterday's confusion.");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.print("# ##### #####  ");
+        System.out.println("   ##### ##### # #   # ##### #####");
+        System.out.print("# #   # #   #     ");
+        System.out.println("#   # #   # # ##  #   #   #         ");
+        System.out.print("# #   # #   #     ");
+        System.out.println("##### #   # # # # #   #   #####");
+        System.out.print("# #   # #   #     ");
+        System.out.println("#     #   # # #   #   #      #");
+        System.out.print("# ##### #####     ");
+        System.out.print("#     ##### # #   #   #   #####");
 
     }
 
